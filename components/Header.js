e784d0c029cf2faf92a95ad769c3d564a207d7e7
Saurabh <saurabhbhoi8@gmail.com>
2022-05-14 14:01:44 +0530

    3rd change

--- components/Header.js
+++ components/Header.js
@@ -1,25 +1,77 @@
 import React from 'react'
 import Image from 'next/image'
-
+import Search from '../assets/svg/search'
 const styles = {
-    header: 'bg-[#17171A] text-white h-20 flex gap-[100px] w-full p-[30px]',
+   header : 'bg-[#17171A] text-white h-20 flex gap-[100px] w-full p-[30px]',
+   headerWrapper : `flex justify-center h-full max-w-screen-xl mx-auto px-4`,
+   nav: 'flex justif-center items-center gap-[20px]',
+   navItem : 'relative mr-1 cursor-pointer hover:opacity-60',
+   badge :' rounded-full bg-blue-600 h-1 w-1 absolute bottom-5 right-0 top-1 ring-4',
+   inputcontainer : 'flex items-center justify-center p-2 rounded bg-[#171924]',
+   input:'bg-transparent outline-none text-white w-70 ml-3 ',
 }
 const Header = () => {
   return (
     <div className={styles.header} >
     <Image
-    src='https://s2.coinmarketcap.com/
-    static/cloud/img/coinmarketcap_white_1.svg'
-    alt='logo'
+    src='https://s2.coinmarketcap.com/static/cloud/img/coinmarketcap_white_1.svg'
+    alt='coinmarketcapp'
     width={220}
     height={220}
     />
 
-    <div className={styles.headerWrapper}>
-      <div className={styles.nav}>
+  <div className={styles.headerWrapper}>
+        <div className={styles.nav}>
+          <div className= { styles.navItem}>
+            <div className={styles.navLink} > Cryptocurrencies </div>
+            <div className={styles.badge}/>
+          </div>
+
+          <div className= { styles.navItem}>
+            <div className={styles.navLink} > Exchanges </div>
+            <div className={styles.badge}/>
+          </div>
+
+          <div className= { styles.navItem}>
+            <div className={styles.navLink} > NFT </div>
+            <div className={styles.badge}/>
+          </div>
+
+          <div className= { styles.navItem}>
+            <div className={styles.navLink} > Cryptown </div>
+            <div className={styles.badge}/>
+          </div>
+
+          <div className= { styles.navItem}>
+            <div className={styles.navLink} > Portfolio </div>
+          </div> 
+
+          <div className= { styles.navItem}>
+            <div className={styles.navLink} > Watchlist  </div>
+          </div>
+
+          <div className= { styles.navItem}>
+            <div className={styles.navLink} > Products </div>
+            <div className={styles.badge}/>
+          </div>
+
+          <div className= { styles.navItem}>
+            <div className={styles.navLink} > Learn  </div>
+          </div>
 
       </div>
-    </div>
+        <div className='flex items-centre'>
+          {/* connect button */}
+          <div className={styles.inputcontainer}>
+           <Search />
+            <input className={styles.input} placeholder ='Search'/>
+          </div>
+        </div>
+
+
+
+
+     </div>
     </div>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ import Header from '../components/Header'
 const Home: NextPage = () => {
   return (
     <div className='min-h-screen'>
-
+        <Header />
       <div className='mt-10'/>
 
       <div className='mt-20'/>
